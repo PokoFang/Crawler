f0af25588129e593bff0b314b2ea9001515beb02
Poko Fang <kogiidae@gmail.com>
2018-10-24 11:40:25 +0800

    add previous page
    Add the function of crawling the previous page

--- crawler.py
+++ crawler.py
@@ -2,17 +2,13 @@ import requests
 from requests_html import HTML
 import pandas as pd
 import numpy as np
+import urllib
 
 def fetch(url):
 	response = requests.get(url) 
 	# response.encoding = 'utf-8'
 	#response = requests.get(url, cookies={'over18': '1'})  # Yes, I am 18 years old.
 	return response
-
-def find_entries(doc):
-	html = HTML(html=doc)
-	post_entries = html.find('div.r-ent')
-	return (post_entries)
 
 def get_infor(entry):
 	return {
@@ -22,12 +18,21 @@ def get_infor(entry):
     'author': entry.find('div.author', first=True).text,
 	}
 
+page_num = 3 # total page number you want to crawl
+
 url = 'https://www.ptt.cc/bbs/TaiwanDrama/index.html'
-resp = fetch(url)
-entries =  find_entries(resp.text)
 
-for entry in entries:
-	print(get_infor(entry))
+for i in range(page_num):
+	print(url)
+	resp = fetch(url)
+	html = HTML(html=resp.text)
+	entries = html.find('div.r-ent')
 
+	for entry in entries: # get information of each post
+		print(get_infor(entry))
 
-	
+	controls = html.find('a.btn.wide')
+	link = controls[1].attrs['href']
+	pre_url = urllib.parse.urljoin('https://www.ptt.cc/', link)
+	url = pre_url
+	i += 1
