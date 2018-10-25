90de372813c6958bf28af04349edeab92b5d0550
Poko Fang <kogiidae@gmail.com>
2018-10-25 13:31:46 +0800

    add voc count
    count voc and output result to a file(under Users)

--- crawler.py
+++ crawler.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import urllib
 import jieba
+import os
 
 def fetch(url):
 	response = requests.get(url) 
@@ -19,10 +20,19 @@ def get_infor(entry):
     'author': entry.find('div.author', first=True).text,
 	}
 
+def count_voc(voc_set, seg_list):
+	for item in seg_list:
+		if item in voc_set:
+			voc_set[item] += 1
+		else:
+			voc_set[item] = 1
+	return voc_set
 
 page_num = 1 # total page number you want to crawl
 
 url = 'https://www.ptt.cc/bbs/TaiwanDrama/index.html'
+del_list = []
+voc_set = {}
 
 for i in range(page_num):
 	print(url)
@@ -34,13 +44,19 @@ for i in range(page_num):
 		entry_dict = get_infor(entry)
 		if '(本文已被刪除)' in entry_dict['title']:
 			entry_dict['author'] = entry_dict['title'][len('(本文已被刪除)') + 2:-1] # 抓出原文作者
+			del_list.append(entry_dict)
+
 		else:
-			seg_list = jieba.lcut(entry_dict['title'], cut_all=False)
-			#print(seg_list)
-		print(entry_dict)
+			seg_list = jieba.cut(entry_dict['title'], cut_all=False)
+			count_voc(voc_set, seg_list)
 
 	controls = html.find('a.btn.wide')
 	link = controls[1].attrs['href']
 	pre_url = urllib.parse.urljoin('https://www.ptt.cc/', link)
 	url = pre_url
 	i += 1
+
+with open ('voc_count.txt', 'w') as out_f: # Users dic
+	for v in voc_set:
+		padding = ' ' * (10 - len(v))
+		out_f.write("%s%s%d\n"%(v, padding, voc_set[v]))
