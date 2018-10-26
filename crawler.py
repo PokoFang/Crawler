72e7fcc7866ccd753524f4cc04e80b788c1a4f33
Poko Fang <kogiidae@gmail.com>
2018-10-26 16:05:22 +0800

    skip class and sort output
    Skip the class of title and sort the counting result

--- crawler.py
+++ crawler.py
@@ -5,6 +5,7 @@ import numpy as np
 import urllib
 import jieba
 import os
+import sys
 
 def fetch(url):
 	response = requests.get(url) 
@@ -28,14 +29,15 @@ def count_voc(voc_set, seg_list):
 			voc_set[item] = 1
 	return voc_set
 
-page_num = 1 # total page number you want to crawl
+page_num = 30 # total page number you want to crawl
 
 url = 'https://www.ptt.cc/bbs/TaiwanDrama/index.html'
 del_list = []
 voc_set = {}
+print("程式預設編碼 : ",sys.getdefaultencoding(), ", cmd預設編碼 : ",sys.stdin.encoding)
 
 for i in range(page_num):
-	print(url)
+	print('page: ', i, ', remaining page number: ', page_num - i)
 	resp = fetch(url)
 	html = HTML(html=resp.text)
 	entries = html.find('div.r-ent')
@@ -43,11 +45,13 @@ for i in range(page_num):
 	for entry in entries: # get information of each post
 		entry_dict = get_infor(entry)
 		if '(本文已被刪除)' in entry_dict['title']:
-			entry_dict['author'] = entry_dict['title'][len('(本文已被刪除)') + 2:-1] # 抓出原文作者
+			entry_dict['author'] = entry_dict['title'][len('(本文已被刪除)') + 2:-1] # find the author ID of a deleted article
 			del_list.append(entry_dict)
 
 		else:
-			seg_list = jieba.cut(entry_dict['title'], cut_all=False)
+			start_idx = entry_dict['title'].find(']') + 2 # skip the article class
+			# print(entry_dict['title'][start_idx: ])
+			seg_list = jieba.cut(entry_dict['title'][start_idx: ], cut_all=False)
 			count_voc(voc_set, seg_list)
 
 	controls = html.find('a.btn.wide')
@@ -56,7 +60,10 @@ for i in range(page_num):
 	url = pre_url
 	i += 1
 
-with open ('voc_count.txt', 'w') as out_f: # Users dic
-	for v in voc_set:
-		padding = ' ' * (10 - len(v))
-		out_f.write("%s%s%d\n"%(v, padding, voc_set[v]))
+sorted_by_value = sorted(voc_set.items(), key=lambda kv: kv[1], reverse=True) # return a list of tuple ('str', num)
+
+# output to a file in Users dic
+with open ('voc_count.txt', 'w', errors='ignore') as out_f: # ignore the character cannot be decoded when writing (it's ok if we only print out)
+	for v_tuple in sorted_by_value:
+		padding = ' ' * (10 - len(v_tuple[0]))
+		out_f.write("%s%s%d\n"%(v_tuple[0], padding, v_tuple[1]))
